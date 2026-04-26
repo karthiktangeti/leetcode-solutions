2dd34e91b477c87a89ac987b81f1d96a55de4d5d
Karthik Tangeti <karthiktangeti11@gmail.com>
2026-04-26 23:06:54 +0530

    Time: 0 ms (100%), Space: 12.4 MB (82.35%) - LeetHub

---
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def queryString(self, s, n):
+        if n > len(s) * 2:
+            return False
+        for i in range(n, n // 2 , -1):
+            if bin(i)[2:] not in s:
+                return False
+        return True
