6d82b10349bfefa65f7c4082ffcf9f4fcce6ceb7
Karthik Tangeti <karthiktangeti11@gmail.com>
2026-05-18 22:43:39 +0530

    Time: 45 ms (12.9%), Space: 13.2 MB (48.39%) - LeetHub

---
+++ 0910-smallest-range-ii/0910-smallest-range-ii.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def smallestRangeII(self, nums, k):
+        nums.sort()
+        n = len(nums)
+        ans = nums[-1] - nums[0]
+        for i in range(n - 1):
+            high = max(nums[-1] - k,nums[i]+k)
+            low = min(nums[0] + k,nums[i + 1] - k)
+            ans = min(ans,high - low)
+        return ans
+        
