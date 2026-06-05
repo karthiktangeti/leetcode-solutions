3a6fe9d52df8d714e218450928b4ac4f259b842e
Karthik Tangeti <karthiktangeti11@gmail.com>
2026-06-05 11:48:16 +0530

    Time: 0 ms (100%), Space: 12.3 MB (62.77%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,12 +1,10 @@
 class Solution(object):
     def generate(self, numRows):
+        row = []
         tri = []
         for i in range(numRows):
             row = [1] * (i + 1)
             for j in range(1,i):
-                row[j] = tri[i - 1][j -1] + tri[i - 1][j]
+                row[j] = tri[i - 1][j- 1] + tri[i - 1][j] 
             tri.append(row)
         return tri
-            
-
-        
