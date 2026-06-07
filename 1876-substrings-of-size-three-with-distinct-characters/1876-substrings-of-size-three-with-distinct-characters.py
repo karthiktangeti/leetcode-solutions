5c947330aab341be6e31de9132fb52de2657e43b
Karthik Tangeti <karthiktangeti11@gmail.com>
2026-06-07 11:05:02 +0530

    Time: 3 ms (46.63%), Space: 12.4 MB (53.97%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -2,12 +2,15 @@ class Solution(object):
     def countGoodSubstrings(self, s):
         n = len(s)
         ans = 0
+        l= 0
+        temp = []
         for i in range(n):
-            for j in range(i,n):
-                temp = []
-                for k in range(i,j + 1):
-                    temp.append(s[k])
-                if len(temp) == 3 and len(set(temp)) == 3:
-                    ans += 1
+            temp.append(s[i])
+            if i - l == 3:
+                temp.pop(0)
+                l += 1
+            if len(temp) == 3 and len(set(temp)) == 3:
+                ans += 1
+
         return ans
         
