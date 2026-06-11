3381ce5ad2cc8375b0ed9ffab00aa8597b4d0948
Karthik Tangeti <karthiktangeti11@gmail.com>
2026-06-11 09:35:46 +0530

    Time: 4 ms (16%), Space: 12.4 MB (8%) - LeetHub

---
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minimumSwap(self, s1, s2):
+        xy = 0
+        yx = 0
+        for a,b in zip(s1,s2):
+            if a == "x" and b == "y":
+                xy += 1
+            elif a == "y" and b == "x":
+                yx += 1
+        if (xy + yx) % 2:
+            return -1
+        return xy // 2 + yx // 2 + 2 * (xy % 2)
