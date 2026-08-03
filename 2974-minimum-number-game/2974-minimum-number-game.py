582666ba55a689adc3c3f0c8f0262cdaef224ef8
Karthik Tangeti <karthiktangeti11@gmail.com>
2026-08-03 08:14:57 +0530

    Time: 7 ms (21.72%), Space: 12.4 MB (17.23%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numberGame(self, nums):
+        nums.sort()
+        for i in range(0,len(nums) - 1,2):
+            nums[i],nums[i + 1] = nums[i + 1],nums[i]
+        return nums
+        
+        
+        
